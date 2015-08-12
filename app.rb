b0b55355c56cc3e76f968b4d5e023dcbeb437104
ryan broughan & tim kellogg <student@epicodus.com>
2015-08-12 15:47:01 -0700

    add index and browsing pages

--- app.rb
+++ app.rb
@@ -1,6 +1,34 @@
 require('sinatra')
 require('sinatra/reloader')
+require './lib/artist'
+require './lib/album'
 
 get('/') do
   erb(:index)
+end
+
+get('/artists') do
+  @artists = Artist.all
+  erb(:artists)
+end
+
+get('/albums') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/add_artist') do
+  erb(:add_artist)
+end
+
+get('/add_album') do
+  erb(:add_album)
+end
+
+post('/artists') do
+  erb(:artists)
+end
+
+post('/albums') do
+  erb(:albums)
 end
